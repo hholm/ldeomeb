1a2c576c6c611495e576a7a21da80840dac1dd92
Henry Holm <32178231+hholm@users.noreply.github.com>
2025-01-22 09:31:23 -0500

    refactoring and adding ft to ph output

--- R/functions.R
+++ R/functions.R
@@ -106,13 +106,13 @@ tidyplate <- function(loc) {
 #' @return A [data.frame] containing calculated pH values with metadata, or a [vector] containing just pH values if verbose == FALSE.
 #' @export
 calc_pH_spec <- function(A730_blank, A578_blank, A434_blank, A730_dye, A578_dye,
-                         A434_dye, vol.dye.L, salinity, verbose = TRUE,
+                         A434_dye, vol.dye.uL, salinity, verbose = TRUE,
                          calibration = calibration) {
   # following hennon formula
   A1_A2 <- (A578_dye - A578_blank - (A730_dye - A730_blank)) / (A434_dye - A434_blank - (A730_dye - A730_blank))
   pK2 <- (1245.9 / 298) + 3.8275 + (0.00211 * (35 - salinity))
   #A1_A2_cor <- A1_A2 + (0.0218 - (0.0359 * A1_A2)) * vol.dye.L
-  A1_A2_cor <- A1_A2 + (-1** vol.dye.L)
+  A1_A2_cor <- A1_A2 + (-1*calibration*vol.dye.uL)
   # return all data if desired
   if (verbose) {
     return(data.frame(A1_A2, pK2, A1_A2_cor, pH = pK2 + log10((A1_A2_cor - 0.00691) / (2.222 - A1_A2_cor * 0.1331))))
@@ -137,9 +137,8 @@ calc_pH_spec <- function(A730_blank, A578_blank, A434_blank, A730_dye, A578_dye,
 #' @export
 calc_plate <- function(tidyplate, verbose = TRUE,
                        pairs = data.frame(blanks = c("A", "C", "E", "G"), dyes = c("B", "D", "F", "H")),
-                       salinity = 35, vol.dye.uL = 3,calibration = -0.00660453) {
-
-  #tidyplate <- tidyplate %>% subset(Mode == "Absorbance")
+                       salinity = 35, vol.dye.uL = 3, calibration = -0.00660453) {
+  # tidyplate <- tidyplate %>% subset(Mode == "Absorbance")
 
   # assign well type based on 'pairs" data.frame
   tidyplate[which(tidyplate$rows %in% pairs$blanks), "type"] <- "blank"
@@ -152,7 +151,7 @@ calc_plate <- function(tidyplate, verbose = TRUE,
   for (i in 1:nrow(pairs)) {
     out[[i]] <- tidyplate %>%
       subset(tidyplate$rows %in% pairs[i, ]) %>%
-      dplyr::mutate(position = paste0(position,paste0(pairs[i, ], collapse = "")))
+      dplyr::mutate(position = paste0(position, paste0(pairs[i, ], collapse = "")))
   }
 
   tidyplate <- do.call(rbind, out)
@@ -167,7 +166,7 @@ calc_plate <- function(tidyplate, verbose = TRUE,
     id_cols <- c("position", "names")
   } else {
     id_cols <- "position"
-    #warning("No names column in data.frame. Using well positions as names.")
+    # warning("No names column in data.frame. Using well positions as names.")
   }
 
   # time issues
@@ -182,69 +181,75 @@ calc_plate <- function(tidyplate, verbose = TRUE,
   return <- return %>%
     mutate(`Average Start Time` = mean(`Start Time`)) %>%
     select(-`Start Time`) %>%
-    select(-c("rows","cols")) %>%
+    select(-c("rows", "cols")) %>%
     tidyr::pivot_wider(names_from = Mode, values_from = value) %>%
-    select(c("Excitation Wavelength","names","type","position","Absorbance","Fluorescence Top Reading","Measurement Wavelength","Average Start Time")) %>%
-    tidyr::pivot_wider(names_from = `Excitation Wavelength`,values_from = `Fluorescence Top Reading`,names_prefix = "Ft_")
+    select(c("Excitation Wavelength", "names", "type", "position", "Absorbance", "Fluorescence Top Reading", "Measurement Wavelength", "Average Start Time")) %>%
+    tidyr::pivot_wider(names_from = `Excitation Wavelength`, values_from = `Fluorescence Top Reading`, names_prefix = "Ft_") %>%
+    select(!contains("Ft_NA"))
 
   # reformat
   return <- return %>%
-    tidyr::pivot_wider(names_from = c(type,`Measurement Wavelength`), values_from = Absorbance)
-  if(class(pull(return,"names")) == "list"){
-    return$names <- pull(return,"names") %>% unlist()
+    tidyr::pivot_wider(names_from = c(type, `Measurement Wavelength`), values_from = Absorbance)
+  if (class(pull(return, "names")) == "list") {
+    return$names <- pull(return, "names") %>% unlist()
   }
 
   return <- return %>%
-    group_by(names,position,`Average Start Time`) %>%
-    summarise(across(everything(),function(x){max(x,na.rm = T)}))
+    group_by(names, position, `Average Start Time`) %>%
+    select(!(contains("blank_NA") | contains("dye_NA"))) %>%
+    summarise(across(everything(), function(x) {
+      max(x, na.rm = T)
+    }), .groups = "drop")
 
 
   # reformat ft values if they exist into a wide format, might be a simpler was to do this
-#  if(exists("Fluorescence Top Reading",where = return)){
+  #  if(exists("Fluorescence Top Reading",where = return)){
 
-    # get ft columns to pivot that are different accross 455 and 630
-#    return <- return %>%
-#    tidyr::pivot_wider(names_from = `Excitation Wavelength`, values_from = contains("Fluorescence Top Reading") | contains("Gain")  | contains("Z-Position")) %>%
-#    select(!contains("_NA"))
-#
-#    # cut and rejoin the ft and abs parts
-#    ft_part <- return %>% subset(!is.na(`Emission Wavelength`))
-#    abs_part <- return %>% subset(is.na(`Emission Wavelength`))#
+  # get ft columns to pivot that are different accross 455 and 630
+  #    return <- return %>%
+  #    tidyr::pivot_wider(names_from = `Excitation Wavelength`, values_from = contains("Fluorescence Top Reading") | contains("Gain")  | contains("Z-Position")) %>%
+  #    select(!contains("_NA"))
+  #
+  #    # cut and rejoin the ft and abs parts
+  #    ft_part <- return %>% subset(!is.na(`Emission Wavelength`))
+  #    abs_part <- return %>% subset(is.na(`Emission Wavelength`))#
 
-#    ft_part <- ft_part %>% select(where(~ !all(is.na(.))))
-#    abs_part <- abs_part %>% select(where(~ !all(is.na(.))))
+  #    ft_part <- ft_part %>% select(where(~ !all(is.na(.))))
+  #    abs_part <- abs_part %>% select(where(~ !all(is.na(.))))
 
-    # Identify columns with identical values
-#    common_cols <- intersect(names(ft_part), names(abs_part)) |>
-#      keep(~ identical(ft_part[[.x]], abs_part[[.x]]))
+  # Identify columns with identical values
+  #    common_cols <- intersect(names(ft_part), names(abs_part)) |>
+  #      keep(~ identical(ft_part[[.x]], abs_part[[.x]]))
 
-#    return <- abs_part |>
-#        select(-all_of(common_cols[common_cols != 'names'])) %>%
-#        full_join(ft_part,by = join_by(names), suffix = c(" Abs", " Ft"))
-#  }
+  #    return <- abs_part |>
+  #        select(-all_of(common_cols[common_cols != 'names'])) %>%
+  #        full_join(ft_part,by = join_by(names), suffix = c(" Abs", " Ft"))
+  #  }
 
   pH <- return %>%
-    select(starts_with("blank_7"),starts_with("dye_7"),starts_with("blank_5"),
-           starts_with("dye_5"),starts_with("blank_4"),starts_with("dye_4")) %>%
+    select(
+      starts_with("blank_7"), starts_with("dye_7"), starts_with("blank_5"),
+      starts_with("dye_5"), starts_with("blank_4"), starts_with("dye_4")
+    ) %>%
     dplyr::mutate_all(as.numeric)
 
   # calculate pH
   pH <- calc_pH_spec(
-    A730_blank = pull(select(pH,starts_with("blank_7"))),
-    A578_blank = pull(select(pH,starts_with("blank_5"))),
-    A434_blank = pull(select(pH,starts_with("blank_4"))),
-    A730_dye = pull(select(pH,starts_with("dye_7"))),
-    A578_dye = pull(select(pH,starts_with("dye_5"))),
-    A434_dye = pull(select(pH,starts_with("dye_4"))),
-    vol.dye.L = vol.dye.L,
+    A730_blank = pull(select(pH, starts_with("blank_7"))),
+    A578_blank = pull(select(pH, starts_with("blank_5"))),
+    A434_blank = pull(select(pH, starts_with("blank_4"))),
+    A730_dye = pull(select(pH, starts_with("dye_7"))),
+    A578_dye = pull(select(pH, starts_with("dye_5"))),
+    A434_dye = pull(select(pH, starts_with("dye_4"))),
+    vol.dye.uL = vol.dye.uL,
     salinity = salinity,
     verbose = verbose,
     calibration = calibration
   )
 
-  #names fix idk why this is happening
-  if(class(pull(return,"names")) == "list"){
-    return$names <- pull(return,"names") %>% unlist()
+  # names fix idk why this is happening
+  if (class(pull(return, "names")) == "list") {
+    return$names <- pull(return, "names") %>% unlist()
   }
 
   # return just pH column or all metadata
@@ -252,7 +257,7 @@ calc_plate <- function(tidyplate, verbose = TRUE,
     return <- cbind(return, pH)
   } else {
     return$pH <- pH
-    return <- return[,c(id_cols,"pH")]
+    return <- return[, c(id_cols, "pH")]
   }
   return(return)
 }
